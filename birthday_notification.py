4f56589784a263516a9ae9723bef8c3e0c3659dc
Krop <andrekropes@yandex.ru>
2015-08-23 00:32:33 +0300

    commit 1

--- birthday_notification.py
+++ birthday_notification.py
@@ -1,33 +1,26 @@
 __author__ = 'krop'
 import smtplib
+import sys
+import os
 import datetime
 import time, sched
-import sys, getopt
 import argparse
+import getpass
 from email.mime.text import MIMEText
 
 
-def send_em(msg):
-    me = 'andrekropes@gmail.com'
-    you = 'andrekropes@yandex.ru'
-    msg['Subject'] = 'Incoming party notification'
-    msg['From'] = you
-    msg['To'] = me
-
-    # Send the message via our own SMTP server, but don't include the
-    # envelope header.
-    s = smtplib.SMTP('smtp.yandex.ru')
+def send_em(msg, serverSMTP, password):
+    s = smtplib.SMTP(serverSMTP)
     s.set_debuglevel(1)
     s.starttls()
-    s.login(you, '555Krop')
-    s.sendmail(you, [me], msg.as_string())
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string())
     s.quit()
 
 
-def get_birth(inputfile,outputfile=None):
+def get_birth(inputfile, send_to=None, outputfile=None, password=None, serverSMTP=None, send_from=None):
     try:
         fp = open(inputfile, 'r')
-
     except:
         return print('Error to open %s' % inputfile)
     a = ''
@@ -39,44 +32,83 @@ def get_birth(inputfile,outputfile=None):
         if birth_time <= datetime.timedelta(days=5) and birth_time >= datetime.timedelta(days=-1):
             z = '%s (%s) - %s days left\n' % (i.split(",")[0], i.split(",")[1], birth_time.days)
             a += z
-    print(a)
     fp.close()
+    msg = MIMEText(a)
+    msg['Subject'] = 'Incoming party notification'
+    msg['From'] = send_from
+    if serverSMTP:
+        msg['To'] = send_to
+        send_em(msg, serverSMTP, password)
+    else:
+        print(msg)
     if outputfile:
+        msg['To'] = send_to
         out_file = open(outputfile, 'w')
-        out_file.write(a)
-        out_file.close()
-        out_file = open(outputfile, 'r')
-        print(out_file.read())
+        out_file.write(str(msg))
         out_file.close()
     else:
-        msg = MIMEText(a)
-        send_em(msg)
-
+        print(msg)
 
 
 def main():
+    print('Birthday notification app\n'
+          'Ctrl+c to exit\n')
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--ifile", type=str,
-                    help="input cvs file ")
+                        help="input cvs file ")
     parser.add_argument("-o", "--ofile", type=str,
-                    help="output cvs file ")
+                        help="output cvs file ")
+    parser.add_argument("-e", "--email", type=str,
+                        help="output email adress ")
     args = parser.parse_args()
-    inputfile=''
-    outputfile=None
+    inputfile = None
+    outputfile = None
+    send_to = None
+    send_from = input('Input From email adress\n')
+    serverSMTP = None
+    password=None
     if args.ifile:
         inputfile = args.ifile
-
+    if args.email:
+        send_to = args.email
+        serverSMTP = input('Input server SMTP server:port or server\n')
+        password = getpass.getpass('Enter email server password\n')
     if args.ofile:
         outputfile = args.ofile
+        if args.email:
+            send_to = args.email
+        else:
+            send_to = input('Input destination email adress\n')
     d = datetime.datetime.now()
-    t = d.replace(hour=1, minute=50, second=00)
+    while True:
+        s = input('Enter space-separated Hour 0-23, Minutes 0-59\n')
+        try:
+            H, M = map(int, s.split())
+        except ValueError:
+            pass
+        else:
+            if 0 <= H < 24 and 0 <= M < 60:
+                break
+            else:
+                print('Try again')
+    t = d.replace(hour=H, minute=M, second=00)
     print(t)
     s = sched.scheduler(time.time, time.sleep)
-    while True:
-        s.enterabs(time.mktime(t.timetuple()), 1, get_birth, (inputfile,outputfile,))
+    while inputfile:
+        s.enterabs(time.mktime(t.timetuple()), 1, get_birth,
+                   (inputfile, send_to, outputfile, password, serverSMTP, send_from,))
         s.run()
         t = t.replace(day=t.day + 1)
+        print('Next alarm at:')
         print(t)
 
+
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nQuit!')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
